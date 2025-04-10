495d2105be310009a985860b96cca8a1e99b3573
Fabriii3G <fabgonzalez@estudiantec.cr>
2025-04-09 19:47:02 -0600

    Set listo

--- Memory_Manager/Client.cpp
+++ Memory_Manager/Client.cpp
@@ -8,11 +8,14 @@
 #pragma comment(lib, "ws2_32.lib")
 
 int main() {
-    MPointer<int>::Init(6060);
-    MPointer<int> ptr = MPointer<int>::New();
-    *ptr = 122323;
-    MPointer<double> ptrDouble = MPointer<double>::New();
+    //MPointer<int>::Init(6060);
+    //MPointer<int> ptr = MPointer<int>::New();
+    //*ptr = 122323;
+    //MPointer<double>::Init(6060);
+    //MPointer<double> ptrDouble = MPointer<double>::New();
+    //*ptrDouble = 122323;
+    MPointer<float>::Init(6060);
     MPointer<float> ptrFloat = MPointer<float>::New();
-
+    *ptrFloat = 122323;
     return 0;
 }
